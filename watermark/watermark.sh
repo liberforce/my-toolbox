b2260d3c50edf579b28c988155e87a7ca95ffc51
Luis Menina <liberforce@freeside.fr>
2022-08-21 18:51:57 +0200

    feat(watermark): add watermarking script

--- watermark/watermark.sh
+++ watermark/watermark.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+# Usage: watermark.sh IMAGE TEXT
+#
+# Goal: Imprints a TEXT on an IMAGE, resulting in a watermarked PDF.
+#       The resulting PDF is generated in the same directory as the source
+#       image.
+
+FILLER_TEXT_FILE="filler.txt"
+
 watermark()
 {
     local input=$1
@@ -11,7 +19,7 @@ watermark()
         -gravity center \
         -pointsize 8 \
         -fill 'rgba(100, 100, 100, 0.25)' \
-        -draw "text 0,0 '$(cat filler.txt)'" \
+        -draw "text 0,0 '$(cat ${FILLER_TEXT_FILE})'" \
         -fill blue \
         -compress zip \
         "${output}"
@@ -28,14 +36,14 @@ create_filler_text()
     local text=$1
     local n_times_on_line=4
     local n_lines=300
-    > filler.txt
+    > "${FILLER_TEXT_FILE}"
     line=$(printf " ${text}%.0s -" $(seq $n_times_on_line))
     line_length="${#line}"
     let wrap_index=${line_length}-1
 
     for i in $(seq $n_lines); do
         line="${line: -1}${line:0:$wrap_index}"
-        echo -e "$line\n" >> filler.txt
+        echo -e "$line\n" >> "${FILLER_TEXT_FILE}"
     done
 }
 
@@ -45,7 +53,7 @@ main()
     outname=$(get_outname "${inname}")
     text=$2
 
-    create_filler_text "$2"
+    create_filler_text "${text}"
     watermark "${inname}" "${outname}"
 }
 
